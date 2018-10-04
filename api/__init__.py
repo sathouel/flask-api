27452b8d006754ce0a0abe0113ec73196b4e2eb2
steven athouel <sathouel@gmail.com>
2018-10-04 19:33:59 +0300

    gitignore updated

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from api.config import Config
+from api.errors import error_response
 
 
 db = SQLAlchemy()
@@ -14,6 +15,10 @@ def create_app(config_class=Config):
     app.config.from_object(config_class)
     db.init_app(app)
     migrate.init_app(app, db)
+
+    @app.errorhandler(404)
+    def not_found():
+        return error_response(404, 'Not found')
 
     from api.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
--- test.py
+++ test.py
@@ -19,10 +19,57 @@ class TestCase(unittest.TestCase):
         db.drop_all()
         self.app_context.pop()
 
-    def test_hello(self):
-        resp = self.client.get('/auth/tst')
-        print(resp)
-        self.assertEqual(resp.status_code, 200)
+    def testRegisterUser(self):
+        u1 = {
+            'username': 'michel',
+            'email': 'michel@tst.com',
+            'password': 'tstpwd'
+        }
+        u2 = {
+            'username': 'michel',
+            'email': 'test@test.com',
+            'password': 'tstpwd'
+        }
+        u3 = {
+            'username': 'jean',
+            'email': 'michel@tst.com',
+            'password': 'tstpwd'
+        }
+        error_message1 = 'please use a different username'
+        error_message2 = 'please use a different email'
+        error_message3 = 'no data provided'
+
+        # Working case
+        resp = self.client.post('/auth/register', json=u1)
+        self.assertEqual(resp.status_code, 201)
+        user = User.query.filter_by(username='michel').first()
+        self.assertEqual(user.username, u1['username'])
+        self.assertEqual(user.email, u1['email'])
+        users = User.query.all()
+        self.assertEqual(len(users), 1)
+
+        # Same username
+        resp2 = self.client.post('/auth/register', json=u1)
+        resp3 = self.client.post('/auth/register', json=u2)
+        resp4 = self.client.post('/auth/register', json=u3)
+        resp5 = self.client.post('/auth/register', json={})
+
+        self.assertEqual(resp2.status_code, 400)
+        self.assertEqual(resp2.json['message'], error_message1)
+
+        self.assertEqual(resp3.status_code, 400)
+        self.assertEqual(resp3.json['message'], error_message1)
+
+        self.assertEqual(resp4.status_code, 400)
+        self.assertEqual(resp4.json['message'], error_message2)
+
+        self.assertEqual(resp5.status_code, 400)
+        self.assertEqual(resp5.json['message'], error_message3)
+
+        users = User.query.all()
+        self.assertEqual(len(users), 1)
+
+
 
 
 if __name__ == '__main__':
